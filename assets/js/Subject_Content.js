3848eec81b9acee0884a9d01aa52f928df0dab14
tmtien2911@gmail.com <tmtien2911@gmail.com>
2022-09-07 15:20:05 +0700

    Cập nhật Baihoc_Noidung

--- assets/js/Subject_Content.js
+++ assets/js/Subject_Content.js
@@ -1,3 +1,4 @@
+
 $.fn.extend({
     treed: function (o) {
 
@@ -291,4 +292,31 @@ $('.nav-bar_menu').click(function(){
 $('.main-content_lesson').click(function(){
     $(".main-content_tree").removeClass('showTree');
     $(".main-content_lesson").removeClass('blurLesson');
+    $(".header-Acount_category").removeClass('show-Category');
+});
+$('.header-General_left').click(function(){
+    $(".header-Acount_category").removeClass('show-Category');
+})
+$('.nav-bar').click(function(){
+    $(".header-Acount_category").removeClass('show-Category');
+})
+//MB
+$('.header-Action_acount').click(function () {
+    $('.header-Acount_category').toggleClass('show-Category');
+});
+//Mobile
+// if(screen.width == 1020){
+//     $('.header-GeneralMB').removeClass('showCategoryMB')
+// }
+$('.header-Menu > i').click(function(){
+    $('.header-GeneralMB').toggleClass('showCategoryMB');
+});
+$('.header-Top_cancel').click(function(){
+    $('.header-GeneralMB').removeClass('showCategoryMB');
+});
+$('#back-top a').click(function () {
+    $('body,html').animate({
+      scrollTop: 0
+    }, 800);
+    return false;
 });
