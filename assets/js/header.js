5455142f34d9cb1df09bbb35c935cc2a8a977430
tmtien2911@gmail.com <tmtien2911@gmail.com>
2022-10-21 16:07:59 +0700

    new

--- assets/js/header.js
+++ assets/js/header.js
@@ -2,45 +2,61 @@ $('.nav-bar_menu').click(function(){
     $(".main-content_tree").toggleClass('showTree');
     $(".main-content_lesson").toggleClass('blurLesson'); 
 });
-$('.main-content_lesson').click(function(){
-    $(".main-content_tree").removeClass('showTree');
-    $(".main-content_lesson").removeClass('blurLesson');
-    $(".header-Acount_category").removeClass('show-Category');
-    $('.header-Acount_icon').removeClass('rote90');
-});
-$('.header-General_left').click(function(){
-    $(".header-Acount_category").removeClass('show-Category');
-    $('.header-Acount_icon').removeClass('rote90');
-})
-$('.nav-bar').click(function(){
-    $(".header-Acount_category").removeClass('show-Category');
-    $('.header-Acount_icon').removeClass('rote90');
-})
-$('.header-Acount_category').mouseover(function(){
-    $(".header-Acount_category").addClass('show-Category');
-    $('.header-Acount_icon').addClass('rote90');
-});
-$('.header-Acount_category').mouseout(function(){
-    $(".header-Acount_category").removeClass('show-Category');
-    $('.header-Acount_icon').removeClass('rote90');
-});
-//MB
-$('.header-Action_acount').click(function () {
-    $('.header-Acount_category').toggleClass('show-Category');
-    $('.header-Acount_icon').toggleClass('rote90');
-});
-if($('.header-Acount_icon').css('transform','rotate(90)') >=1 ){
-    $(".header-Acount_category").removeClass('show-Category');
+// $('.main-content_lesson').click(function(){
+//     $(".main-content_tree").removeClass('showTree');
+//     $(".main-content_lesson").removeClass('blurLesson');
+//     $(".header-Acount_category").removeClass('show-Category');
+//     $('.header-Acount_icon').removeClass('rote90');
+// });
+// $('.header-General_left').click(function(){
+//     $(".header-Acount_category").removeClass('show-Category');
+//     $('.header-Acount_icon').removeClass('rote90');
+// })
+// $('.nav-bar').click(function(){
+//     $(".header-Acount_category").removeClass('show-Category');
+//     $('.header-Acount_icon').removeClass('rote90');
+// })
+
+const onMouseUpDesk = e => {
+    if (!$(".header-Acount_category").is(e.target) // If the target of the click isn't the container...
+        && $(".header-Acount_category").has(e.target).length === 0); // ... or a descendant of the container.
+    {
+        $(".header-Acount_category").removeClass('show-Category');
+        $('.header-Acount_icon').removeClass('rote90');
+
+    }
 }
-//Mobile
-$('.header-Menu > i').click(function(){
-    $('.header-GeneralMB').toggleClass('showCategoryMB');
+
+$('.header-Action_acount').on('click', () => {
+    // $('.header-Acount_icon').addClass('rote90');
+    $(".header-Acount_category").toggleClass('show-Category');
+    if (!$(".header-Acount_category").is(e.target) // If the target of the click isn't the container...
+        && $(".header-Acount_category").has(e.target).length === 0); // ... or a descendant of the container.
+    {
+        $(".header-Acount_category").removeClass('show-Category');
+    }
+    $('.header-Acount_icon').removeClass('rote90');
+      
 });
 
-$('.header-Top_cancel').click(function(){
-    $('.header-GeneralMB').removeClass('showCategoryMB');
+//Mobile
+const $menuMB = $('.header-GeneralMB');
+
+const onMouseUpMB = e => {
+    if (!$menuMB.is(e.target) // If the target of the click isn't the container...
+        && $menuMB.has(e.target).length === 0); // ... or a descendant of the container.
+    {
+        $menuMB.removeClass('showCategoryMB');
+    }
+}
+
+$('.header-Menu > i').on('click', () => {
+    $menuMB.toggleClass('showCategoryMB').promise().done(() => {
+        if ($menuMB.hasClass('showCategoryMB')) {
+            $(document).on('mouseup', onMouseUpMB); // Only listen for mouseup when menu is active...
+        } else {
+            $(document).off('mouseup', onMouseUpMB); // else remove listener.
+        }
+    })
 });
-$('section').click(function(){
-    $('.header-GeneralMB').removeClass('showCategoryMB');
-    $('.header-Acount_category').removeClass('show-Category');
-});
+
