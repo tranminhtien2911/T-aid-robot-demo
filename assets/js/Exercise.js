d955f2982ff41d7275c37e925bf010dc177cec6a
tmtien2911@gmail.com <tmtien2911@gmail.com>
2022-12-13 23:42:47 +0700

    time

--- assets/js/Exercise.js
+++ assets/js/Exercise.js
@@ -132,6 +132,28 @@ $(document).ready(function () {
             }
         }
     })
+    //countdown
+    let countdown = $('.ex-submit-time > span').text();
+    let interval = setInterval(function () {
+        let timer = countdown.split(':');
+        //by parsing integer, I avoid all extra string processing
+        var minutes = parseInt(timer[0], 10);
+        var seconds = parseInt(timer[1], 10);
+        --seconds;
+        minutes = (seconds < 0) ? --minutes : minutes;
+        if (minutes < 0) clearInterval(interval);
+        seconds = (seconds < 0) ? 59 : seconds;
+        seconds = (seconds < 10) ? '0' + seconds : seconds;
+        //minutes = (minutes < 10) ?  minutes : minutes;
+        countdown = minutes + ':' + seconds;
+        $('.ex-submit-time > span').html(minutes + ':' + seconds);
+        if(minutes == 0 && (seconds <= 59)){
+            $('.ex-submit-time').addClass('ex-timesUp');
+        }
+    }, 1000);
+
+
+
 
 
 });
